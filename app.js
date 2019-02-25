765029fa0d70b5976baef70f5c4001a391ca2baa
COR Students <corstudents@Matts-MBP-22.localdomain>
2019-02-25 13:50:48 -0500

    port update

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
